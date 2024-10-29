57a45dbd90e3e412e657d2e07be499d8748b93a8
sergio <serhiy_kovalchuk@race.es>
2024-10-29 19:14:36 +0100

    finalizada lección 444

--- Router.php
+++ Router.php
@@ -2,23 +2,20 @@
 
 namespace MVC;
 
-class Router
-{
+class Router {
+    
     public array $getRoutes = [];
     public array $postRoutes = [];
 
-    public function get($url, $fn)
-    {
+    public function get($url, $fn) {
         $this->getRoutes[$url] = $fn;
     }
 
-    public function post($url, $fn)
-    {
+    public function post($url, $fn) {
         $this->postRoutes[$url] = $fn;
     }
 
-    public function comprobarRutas()
-    {
+    public function comprobarRutas() {
         
         // Proteger Rutas...
         session_start();
@@ -46,8 +43,7 @@ class Router
         }
     }
 
-    public function render($view, $datos = [])
-    {
+    public function render($view, $datos = []){
 
         // Leer lo que le pasamos  a la vista
         foreach ($datos as $key => $value) {
