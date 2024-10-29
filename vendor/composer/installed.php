04322656dbdc32bf20f80665ee4b87ec6f345cfb
sergio <serhiy_kovalchuk@race.es>
2024-10-29 18:09:21 +0100

    leccion 443 terminada - 1

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'kovalchuk/appsalon',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '387cff989aec95efe7f76d6f8ff595624937ea61',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'kovalchuk/appsalon' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '387cff989aec95efe7f76d6f8ff595624937ea61',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
